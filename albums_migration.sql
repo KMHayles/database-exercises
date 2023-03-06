438ead17653b2ea4f505a582aae66fb1268930da
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-03-06 11:01:08 -0600

    mysql exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
-USE codeup_test_db;
+Use codeup_test_db;
 
 DROP TABLE albums;
 
 CREATE TABLE albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(100) NOT NULL,
+  artist VARCHAR(102) NOT NULL,
   name VARCHAR(100),
   release_date SMALLINT UNSIGNED,
   sales FLOAT,
