5591899f60c78288dd68777f29d667922fc327c4
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-03-15 02:45:58 -0500

    add mysql exercises

---
+++ update_exercises.sql
@@ -0,0 +1,26 @@
+USE codeup_test_db;
+
+SELECT name AS 'All albums in the table'
+FROM albums;
+
+SELECT name AS 'albums released before 1980'
+FROM albums
+WHERE release_date < 1980;
+
+SELECT name AS 'albums by Michael Jackson'
+FROM albums
+WHERE artist = 'Michael Jackson';
+
+UPDATE albums
+SET sales = sales * 10;
+SELECT * FROM albums;
+
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+
+SELECT * FROM albums;
